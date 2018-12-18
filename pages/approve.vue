b20a8b40e44105b3d3b0edb3558ff4b13e6751c8
crigger <adam@crig.ga>
2018-12-18 09:54:17 -0500

    Adjust sum formids.

--- pages/approve.vue
+++ pages/approve.vue
@@ -8,7 +8,7 @@
       <ul>
         <li v-for="(memory, index) in content" :key="index">
 
-          <form name="approve" action="/thank-you" netlify-honeypot="bot-field" method="post" netlify>
+          <form name="approve" netlify-honeypot="bot-field" method="post" netlify>
             <input type="hidden" name="form-name" value="approve">
             <!-- <p class="hidden">
               <label>Don’t fill this out: <input name="bot-field"></label>
@@ -16,18 +16,18 @@
             <label class="form-label" for="name">
               Name:
             </label>
-            <input id="name" :value="memory.name" class="form-field" name="name">
+            <input :id="'name-' + memory.id" :value="memory.name" class="form-field" name="name">
             <label class="form-label" for="email">
               Email:
             </label>
-            <input id="email" :value="memory.email" class="form-field" name="email" >
+            <input :id="'email-' + memory.id" :value="memory.email" class="form-field" name="email" >
             <label class="form-label" for="message">
               Message:
             </label>
-            <textarea id="message" :value="memory.summary" class="form-field" name="message" />
+            <textarea :id="'message-' + memory.id" :value="memory.summary" class="form-field" name="message" />
 
             <img :src="memory.ordered_human_fields[3].value">
-            <input id="avatar" :value="memory.ordered_human_fields[3].value" name="avatar">
+            <input :id="'avatar-' + memory.id" :value="memory.ordered_human_fields[3].value" name="avatar">
             <input class="form-button" type="submit" value="Approve">
           </form>
 
